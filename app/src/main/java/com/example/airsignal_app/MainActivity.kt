12978d56b69734e04748157291efd59957c602f3
JaeYoung <tekken5953@naver.com>
2023-03-16 17:13:33 +0900

    2023-03-16 1. 와이어 프레임 작성중

--- app/src/main/java/com/example/airsignal_app/MainActivity.kt
+++ app/src/main/java/com/example/airsignal_app/MainActivity.kt
@@ -1,54 +1,75 @@
 package com.example.airsignal_app
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
-import com.example.airsignal_app.IgnoredKeyFile.lastLoginPhone
-import com.example.airsignal_app.IgnoredKeyFile.lastLoginPlatform
 import com.example.airsignal_app.databinding.ActivityMainBinding
-import com.example.airsignal_app.login.GoogleLogin
-import com.example.airsignal_app.login.KakaoLogin
-import com.example.airsignal_app.login.NaverLogin
-import com.example.airsignal_app.util.SharedPreferenceManager
-
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val googleLogin: GoogleLogin by lazy { GoogleLogin(this) }
-    private val kakakoLogin: KakaoLogin by lazy { KakaoLogin(this) }
-    private val naverLogin: NaverLogin by lazy { NaverLogin(this) }
-    private var phoneNumber: String = ""
 
+    override fun onResume() {
+        super.onResume()
+        binding.mainScrollView.bringToFront()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
-        val lastLogin = SharedPreferenceManager(this).getString(lastLoginPlatform)
-
-        @SuppressLint("SetTextI18n")
-        binding.currentSort.text = "현재 로그인 : $lastLogin"
-
-        phoneNumber = SharedPreferenceManager(this).getString(lastLoginPhone)
-
-        binding.signOutGoogleButton.setOnClickListener {
-            googleLogin.logout()
+        binding.mainSideMenuIv.setOnClickListener {
+            binding.mainDrawerLayout.apply {
+                openDrawer(GravityCompat.START)
+                clipToPadding = false
+                bringToFront()
+            }
         }
 
-        binding.signOutKakaoButton.setOnClickListener {
-            kakakoLogin.initialize()
-            kakakoLogin.logout(phoneNumber)
+        binding.mainNavView.getHeaderView(0).findViewById<ImageView>(R.id.headerCancel).setOnClickListener {
+            binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+            binding.mainScrollView.bringToFront()
         }
 
-        binding.signOutNaverButton.setOnClickListener {
-            naverLogin.initialize()
-            naverLogin.logout(phoneNumber)
-        }
+        binding.mainNavView.setNavigationItemSelectedListener(object :
+            NavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.side_menu_weather -> {
+                        Toast.makeText(this@MainActivity, "날씨정보", Toast.LENGTH_SHORT).show()
+                        return true
+                    }
+                    R.id.side_menu_device -> {
+                        val intent = Intent(this@MainActivity, MyDeviceActivity::class.java)
+                        startActivity(intent)
+                        if (binding.mainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                            binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+                        }
+                        return true
+                    }
+                    R.id.side_menu_setting -> {
+                        Toast.makeText(this@MainActivity, "앱 설정", Toast.LENGTH_SHORT).show()
+                        return true
+                    }
+                    else -> {
+                        return false
+                    }
+                }
+            }
+        })
+    }
 
-        binding.mainDialogBtn.setOnClickListener {
-            val bottomSheet = AddDeviceDialog()
-            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+    override fun onBackPressed() {
+        if (binding.mainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+            binding.mainScrollView.bringToFront()
+        } else {
+            super.onBackPressed()
         }
     }
 }
---
+++ app/src/main/java/com/example/airsignal_app/TestPageActivity.kt
@@ -0,0 +1,50 @@
+package com.example.airsignal_app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.airsignal_app.IgnoredKeyFile.lastLoginPhone
+import com.example.airsignal_app.IgnoredKeyFile.lastLoginPlatform
+import com.example.airsignal_app.databinding.ActivityTestPageBinding
+import com.example.airsignal_app.login.GoogleLogin
+import com.example.airsignal_app.login.KakaoLogin
+import com.example.airsignal_app.login.NaverLogin
+import com.example.airsignal_app.util.SharedPreferenceManager
+
+
+class TestPageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTestPageBinding
+    private val googleLogin: GoogleLogin by lazy { GoogleLogin(this) }
+    private val kakakoLogin: KakaoLogin by lazy { KakaoLogin(this) }
+    private val naverLogin: NaverLogin by lazy { NaverLogin(this) }
+    private var phoneNumber: String = ""
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_test_page)
+
+        val lastLogin = SharedPreferenceManager(this).getString(lastLoginPlatform)
+
+        @SuppressLint("SetTextI18n")
+        binding.currentSort.text = "현재 로그인 : $lastLogin"
+
+        phoneNumber = SharedPreferenceManager(this).getString(lastLoginPhone)
+
+        binding.signOutGoogleButton.setOnClickListener {
+            googleLogin.logout()
+        }
+
+        binding.signOutKakaoButton.setOnClickListener {
+            kakakoLogin.initialize()
+            kakakoLogin.logout(phoneNumber)
+        }
+
+        binding.signOutNaverButton.setOnClickListener {
+            naverLogin.initialize()
+            naverLogin.logout(phoneNumber)
+        }
+
+    }
+}
